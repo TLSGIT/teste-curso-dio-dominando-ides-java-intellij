f1024b8cc48f6189c3a8e69d8dad80cc2cc9481d
TLSGIT <teles.tux@gmail.com>
2023-03-11 08:00:12 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello gatinha!");
+
+        Gato gato = new Gato();
+    }
+}
